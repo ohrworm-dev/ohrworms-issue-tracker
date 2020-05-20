c60ff7410c8ff5edfd63a0ab118528f3a53b5f18
Ohrworm Dev <ohrworm@Ohrworms-MacBook-Pro.local>
2020-05-20 14:03:06 -0400

    heroku deploy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class='Home'>
+    <h1>Homepage Works</h1>
+    <ApolloQuery :query="require('../graphql/queries/allTodos.gql')">
+      <template v-slot="{result: {error, data}}">
+        <h2 v-if="error">Please try again.</h2>
+        <div v-if="data">
+          <p v-for="todo in data.todos" :key="todo.id">
+            {{todo.title}}
+          </p>
+        </div>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
